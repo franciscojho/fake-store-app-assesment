552e50a0e26c7172743acbb6bd358bb3205fba0d
Francisco Huapaya <franciscojho@hotmail.com>
2021-12-22 13:55:34 -0500

    Add ui component footer and css

---
+++ src/components/ui/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer className={styles.store__footer}>
+            <p className={styles['store__footer-content']}>Created by Franciscojho</p>
+        </footer>
+    )
+}
+
+export default Footer
