d56e5363c8478fab83af041e1fb6f751c7d4a784
Francisco Huapaya <franciscojho@hotmail.com>
2021-12-22 13:52:41 -0500

    Add component about and css

---
+++ src/components/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './about.module.css'
+
+const About = () => {
+    return (
+        <div className={styles.store__about}>
+            <h1>About Page</h1>
+        </div>
+    )
+}
+
+export default About
