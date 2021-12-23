3d31e7896fa0353d3478e13e09fc2e3e884f39f2
Francisco Huapaya <franciscojho@hotmail.com>
2021-12-23 18:32:57 -0500

    Add local reducer

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,23 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
+import Loader from '../ui/loader/Loader'
 import ProductCard from '../ui/product-card/ProductCard'
 import styles from './home.module.css'
 
+const initialState = {
+    isLoading: false,
+    products: [],
+}
+
+const productsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_PRODUCTS':
+            return {
+                isLoading: false,
+                products: action.payload,
+            }
+
+        case 'IS_LOADING':
+            return {
+                isLoading: true,
+                products: [],
+            }
+
+        default:
+            return state
+    }
+}
+
 const Home = () => {
-    const [products, setProducts] = useState([])
+    const [{ isLoading, products }, dispatch] = useReducer(productsReducer, initialState)
     useEffect(() => {
+        dispatch({ type: 'IS_LOADING' })
         fetch('https://fakestoreapi.com/products')
             .then((res) => res.json())
-            .then((json) => setProducts(json))
+            .then((json) => dispatch({ type: 'SET_PRODUCTS', payload: json }))
     }, [])
 
     return (
         <div className={styles.store__home}>
-            <h1>Home Page</h1>
-            <div className={styles['store__home-products']}>
-                {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <h1>Products list</h1>
+                    <div className={styles['store__home-products']}>
+                        {products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     )
 }
