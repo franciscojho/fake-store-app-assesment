a1ccedc548bb29fbd186803d8a65ac8fbdf8266e
Francisco Huapaya <franciscojho@hotmail.com>
2021-12-26 23:28:09 -0500

    Add Loader and github link

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,60 +1,74 @@
 import React, { useEffect, useState } from 'react'
+import Loader from '../ui/loader/Loader'
 import styles from './about.module.css'
 
 const About = () => {
     const [info, setInfo] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         fetch('https://api.github.com/users/franciscojho')
             .then((res) => res.json())
             .then((json) => setInfo(json))
+        setIsLoading(false)
     }, [])
 
     return (
         <div className={styles.store__about}>
-            {/* <h1>About Me</h1> */}
             <div className={styles['store__about-container']}>
-                <div className={styles['store__about-info']}>
-                    <img
-                        className={styles['store__about-avatar']}
-                        src="https://avatars.githubusercontent.com/u/42306296?v=4"
-                        alt=""
-                    />
-                    <h3 className={styles['store__about-name']}>{info.name}</h3>
-                    <h4 className={styles['store__about-login']}>{info.login}</h4>
-                    <p className="store__about-bio">{info.bio}</p>
-                    <ul className={styles['store__about-learnings']}>
-                        <li>
-                            <i className="fas fa-check" />
-                            &nbsp;React
-                        </li>
-                        <li>
-                            <i className="fas fa-check" />
-                            &nbsp;Scrum
-                        </li>
-                        <li>
-                            <i className="fas fa-check" />
-                            &nbsp;Git Workflow
-                        </li>
-                    </ul>
-                    <hr />
-                    <ul className={styles['store__about-details']}>
-                        <li className={styles.detail}>
-                            <i className={`${styles.icon} far fa-building`} />
-                            <span>&nbsp;{info.company}</span>
-                        </li>
-                        <li className={styles.detail}>
-                            <i className={`${styles.icon} fas fa-map-marker-alt`} />
-                            <span>&nbsp;{info.location}</span>
-                        </li>
-                        <li className={styles.detail}>
-                            <i className={`${styles.icon} far fa-envelope`} />
-                            <span>
-                                &nbsp;{!info.email ? 'franciscojho@hotmail.com' : info.email}
-                            </span>
-                        </li>
-                    </ul>
-                </div>
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    <div className={styles['store__about-info']}>
+                        <img
+                            className={styles['store__about-avatar']}
+                            src="https://avatars.githubusercontent.com/u/42306296?v=4"
+                            alt=""
+                        />
+                        <h3 className={styles['store__about-name']}>{info.name}</h3>
+                        <h4 className={styles['store__about-login']}>
+                            <a
+                                href="https://github.com/franciscojho"
+                                className={styles['store__about-login-link']}
+                            >
+                                @{info.login}
+                            </a>
+                        </h4>
+                        <p className="store__about-bio">{info.bio}</p>
+                        <ul className={styles['store__about-learnings']}>
+                            <li>
+                                <i className="fas fa-check" />
+                                &nbsp;React
+                            </li>
+                            <li>
+                                <i className="fas fa-check" />
+                                &nbsp;Scrum
+                            </li>
+                            <li>
+                                <i className="fas fa-check" />
+                                &nbsp;Git Workflow
+                            </li>
+                        </ul>
+                        <hr />
+                        <ul className={styles['store__about-details']}>
+                            <li className={styles.detail}>
+                                <i className={`${styles.icon} far fa-building`} />
+                                <span>&nbsp;{info.company}</span>
+                            </li>
+                            <li className={styles.detail}>
+                                <i className={`${styles.icon} fas fa-map-marker-alt`} />
+                                <span>&nbsp;{info.location}</span>
+                            </li>
+                            <li className={styles.detail}>
+                                <i className={`${styles.icon} far fa-envelope`} />
+                                <span>
+                                    &nbsp;{!info.email ? 'franciscojho@hotmail.com' : info.email}
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
     )
