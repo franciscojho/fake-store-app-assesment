1a87d50d81f70eda9f09b08d86b910eec1bd8a8d
Francisco Huapaya <franciscojho@hotmail.com>
2021-12-26 23:17:13 -0500

    Replace async with useLocation

--- src/components/product-detail/ProductDetail.jsx
+++ src/components/product-detail/ProductDetail.jsx
@@ -1,49 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import ReactStars from 'react-rating-stars-component'
-import { useParams } from 'react-router-dom'
-import Loader from '../ui/loader/Loader'
+import { useLocation } from 'react-router-dom'
 import styles from './productDetail.module.css'
 
-const getProduct = async (id) => {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`)
-    const data = await response.json()
-    return data
-}
-
 const ProductDetail = () => {
-    const { id } = useParams()
-    const [product, setProduct] = useState({})
-    const [isLoading, setIsLoading] = useState(false)
-
-    useEffect(() => {
-        setIsLoading(true)
-        async function fetch() {
-            const response = await getProduct(id)
-            setProduct(response)
-            setIsLoading(false)
-        }
-        fetch()
-    }, [id])
+    const location = useLocation()
+    const { product } = location.state
 
     return (
         <div className={styles['store__prod-detail']}>
-            {isLoading ? (
-                <Loader />
-            ) : (
-                <>
-                    <img className={styles['store__prod-detail-img']} src={product.image} alt="" />
-                    <div className={styles['store__prod-detail-content']}>
-                        <h1 className={styles['store__prod-detail-title']}>{product.title}</h1>
-                        <ReactStars edit={false} count={5} value={product.rating?.rate} />
-                        <p>Price: ${product.price}</p>
-                        <h4>About this item</h4>
-                        <p>{product.description}</p>
-                        <ul>
-                            <li>Cateogry: {product.category}</li>
-                        </ul>
-                    </div>
-                </>
-            )}
+            <img className={styles['store__prod-detail-img']} src={product.image} alt="" />
+            <div className={styles['store__prod-detail-content']}>
+                <h1 className={styles['store__prod-detail-title']}>{product.title}</h1>
+                <ReactStars edit={false} count={5} value={product.rating?.rate} />
+                <p>Price: ${product.price}</p>
+                <h4>About this item</h4>
+                <p>{product.description}</p>
+                <ul>
+                    <li>Cateogry: {product.category}</li>
+                </ul>
+            </div>
         </div>
     )
 }
